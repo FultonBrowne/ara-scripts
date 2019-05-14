87507a57af4120c8bf7f8cb0519bed4421cdef46
andromeda-software <gprlabs@outlook.com>
2019-05-14 14:48:35 +0000

    added some stuff

---
+++ src/main.py
@@ -0,0 +1,8 @@
+def main():
+    welc = "welcome to ara version:"
+    ver = "0.0.0.0 in the works"
+    prompt = "enter command>>>>"
+    print(welc)
+    print(ver)
+    rec = input(prompt)
+main()
